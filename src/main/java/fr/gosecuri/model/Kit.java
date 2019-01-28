e51a27115a7642c1b6e968041ccab527d599c507
Adrien <adrienm.martineau@gmail.com>
2019-01-28 09:21:07 +0100

    Edit conf and models

--- src/main/java/fr/gosecuri/model/Kit.java
+++ src/main/java/fr/gosecuri/model/Kit.java
@@ -3,5 +3,15 @@ package fr.gosecuri.model;
 import fr.gosecuri.config.Firebase;
 
 public class Kit extends Firebase {
+    public String name;
 
+    public Integer quantity;
+
+    public Kit() {}
+
+    public Kit(String name) {
+        this.name = name;
+
+        this.quantity = Integer.parseInt(this.get(name));
+    }
 }
