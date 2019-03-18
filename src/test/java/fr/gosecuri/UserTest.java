6bb03c4516b1f81afb9824b4c5ac1c0be07bddcd
Fabien RENAUD <frd.frenaud@gmail.com>
2019-03-18 10:03:21 +0100

    JUnit
    JUnit

--- src/test/java/fr/gosecuri/UserTest.java
+++ src/test/java/fr/gosecuri/UserTest.java
@@ -1,21 +1,13 @@
 package fr.gosecuri;
 
-        import org.jboss.arquillian.container.test.api.Deployment;
-        import org.jboss.arquillian.junit.Arquillian;
-        import org.jboss.shrinkwrap.api.ShrinkWrap;
-        import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-        import org.jboss.shrinkwrap.api.spec.JavaArchive;
-        import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-        import static org.junit.Assert.*;
-
-@RunWith(Arquillian.class)
 public class UserTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(User.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    @Test
+    public void createUser() {
+        User user = new User("01/01/2000","Full Name");
+        assertEquals("User created", "Full Name", user.full_name);
     }
 
 }
